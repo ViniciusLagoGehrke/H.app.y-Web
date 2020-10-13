3a82e0827cd3d1421cf1999773d2f4fe8ebd9df0
Vinicius Lago Gehrke <vinictus@gmail.com>
2020-10-13 23:26:46 +0000

    finished layout and added TileLayers on map

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -33,7 +33,8 @@ de muitas crianças :)</p>
         style={{ width:'100%', height: '100%' }}
       >
         {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" /> */}
-        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/titles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN }`}/>
+        <TileLayer
+          url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
       </Map>
 
       <Link to="" className="create-orphanage">
