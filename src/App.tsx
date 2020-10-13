f7dc3004caf3442af249cf33cbfa3ea54892f0ed
Vinicius Lago Gehrke <vinictus@gmail.com>
2020-10-13 13:20:53 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+function App() {
+  return (
+    <div id="page-landing>
+    <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
